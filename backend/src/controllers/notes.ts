d9778d8a9e7e01f05b642aec792563bc1db69b81
Charles Cruse <charle9@vt.edu>
2023-09-19 15:45:58 -0400

    added error handling for creating and fetching notes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import NoteModel from '../models/note'
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
     try {  
@@ -16,18 +18,37 @@ export const getNote: RequestHandler = async (req, res, next) => {
     const noteId = req.params.noteId;
     
     try {
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id")
+        }
+
         const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {
+            throw createHttpError(404, "Note not found")
+        }
         res.status(200).json(note)
     } catch (error) {
         next(error)
     }
 }
 
-export const createNote: RequestHandler = async (req, res, next) => {
+interface CreateNoteBody {
+    title?: string,
+    text?: string,
+}
+
+//third argument is the body, which we want to use our interface against, other three are req, res, next 
+//we will pass unknown here instead of any since unknown is more restrictive
+export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
     
     try {
+        if (!title) {
+            throw createHttpError(400, "Note must have a title")
+        }
+
         const newNote = await NoteModel.create({
             title: title,
             text: text,
