a23e16b70f7054a2423f13cfd1cdd7f51081c1a2
Charles Cruse <charle9@vt.edu>
2023-09-19 15:16:21 -0400

    added HTTP request error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,28 +1,33 @@
 import "dotenv/config"
 import express, { NextFunction, Request, Response } from 'express';
-import NoteModel from './models/note'
+import notesRoutes from "./routes/notes";
+import morgan from "morgan"
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
+app.use(morgan("dev"))
 
-app.get('/', async (req, res, next) => {
-    try {  
-        // throw Error("Error")
-        const notes = await NoteModel.find().exec();
-        res.status(200).json(notes);
-    } catch(error) {
-        next(error);
-    }
-});
+app.use(express.json())
 
+app.use("/api/notes", notesRoutes)
+
+//We want this route & middleware here to catch when users go to endpoints we don't define, which is why we have no endpoint listed, but we want before the error handler so that we can keep forwarding the error
+app.use((req, res, next) => {
+    next(createHttpError(404, "Endpoint not found"))
+})
 
 //Even if next is not used, express needs these 4 arguments or it cant use this use method
-//.get and .use both have middlewear that actually creates our endpoints for stuff, but error middlewear has to be specified
+//.get and .use both have middlewear that actually creates our endpoints for stuff, but error middleware has to be specified
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.log(error);
     let errorMessage = "An unkonwn error occured";
-    if (error instanceof Error) errorMessage = error.message
-    res.status(500).json({error: errorMessage})
+    let statusCode = 500
+    if (isHttpError(error)) {
+        statusCode = error.status
+        errorMessage = error.message
+    } 
+    res.status(statusCode).json({error: errorMessage})
 })
 
 export default app;
