d587548a2074703c1f89508da2a1849627261f5c
Charles Cruse <charle9@vt.edu>
2023-09-19 16:02:40 -0400

    added backend logic for updating notes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -39,7 +39,7 @@ interface CreateNoteBody {
 }
 
 //third argument is the body, which we want to use our interface against, other three are req, res, next 
-//we will pass unknown here instead of any since unknown is more restrictive
+//we will pass unknown here instead of any since unknown is more restrictive, since 4 values MUST be passed, all or none, can't just do one
 export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
@@ -55,6 +55,49 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
         });
 
         res.status(201).json(newNote);
+    } catch (error) {
+        next(error)
+    }
+}
+
+//noteId has to match what is in the route in terms of spelling
+interface UpdateNoteParams {
+    noteId: string,
+}
+
+interface UpdateNoteBody {
+    title?: string,
+    text?: string
+}
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBody, unknown> = async (req, res, next) => {
+    const noteId = req.params.noteId
+    const newTitle = req.body.title
+    const newText = req.body.text
+
+    
+    try {
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id")
+        }
+
+        if (!newTitle) {
+            throw createHttpError(400, "Note must have title")
+        }
+
+        const note = await NoteModel.findById(noteId).exec()
+
+        if (!note) {
+            throw createHttpError(404, "Note not found")
+        }
+
+        note.title = newTitle
+        note.text = newText
+
+        //we want to update the note, so we don't have to do a fetch again later
+        const updatedNote = await note.save();
+
+        res.status(200).json(updatedNote)
     } catch (error) {
         next(error)
     }
