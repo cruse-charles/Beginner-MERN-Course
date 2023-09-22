c363b1cdcde12683fa9e6a061babbda11e2549c3
Charles Cruse <charle9@vt.edu>
2023-09-21 23:01:07 -0400

    added note form input fields

--- frontend/src/components/AddNoteDialoge.tsx
+++ frontend/src/components/AddNoteDialoge.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 interface AddNoteDialogeProps {
   onDismiss: () => void;
@@ -11,6 +11,24 @@ const AddNoteDialoge = ({ onDismiss }: AddNoteDialogeProps) => {
       <Modal.Header closeButton>
         <Modal.Title>Add Note</Modal.Title>
       </Modal.Header>
+
+      <Modal.Body>
+        <Form id="addNoteForm">
+          <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Title" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Text</Form.Label>
+            <Form.Control as="textarea" rows={5} placeholder="Text" />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button type="submit" form="addNoteForm">
+          Save
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
